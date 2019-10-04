12b5ca820eb6ab9cc081db76002575b4e7d90c26
brunacarbonesi <brunacarbonesi@gmail.com>
2019-10-03 22:36:59 -0300

    Initial commit

---
+++ app/src/main/java/com/bcarbonesi/cineapp_atc/View/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.bcarbonesi.cineapp_atc.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcarbonesi.cineapp_atc.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
